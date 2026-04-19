c5d7824a4183c82f51fea4b2957ef2631249ff4f
Amy <amythest75@icloud.com>
2026-04-19 20:29:45 +0100

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,108 @@
+var none = 0;
+var max = Number.MAX_SAFE_INTEGER;
+var min = Number.MIN_SAFE_INTEGER;
+
+// TODO: Make chain number ++ or -- instead of using incrementCounterValueBy
+
+function clickPlus() {
+    incrementCounterValueBy(1);
+    updateMessage();
+}
+
+function clickMinus() {
+    incrementCounterValueBy(-1);
+    updateMessage();
+}
+
+function incrementCounterValueBy(amount) {
+    var currentValue = getCounterValue();
+    if (currentValue === max || currentValue === min) {
+        return;
+    }
+    getCounterElement().innerText = currentValue + amount
+}
+
+function getCounterElement() {
+    return document.querySelector('#counterValue');
+}
+
+function getCounterValue() {
+    var counterElement = getCounterElement();
+    return Number(counterElement.innerText);
+}
+
+function displayMessage(msg) {
+    var messageBox = document.querySelector('#messageBox');
+    if(messageBox.innerText !== msg) {
+        messageBox.innerText = msg;
+    }
+}
+
+function updateMessage() {
+    var counterValue = getCounterValue();
+    var msg = getMessageFor(counterValue);
+    displayMessage(msg);
+}
+
+// Refactored and added DRY concepts (No more ugly else if's!)
+
+function getMessageFor(counterValue) {
+    switch (counterValue < 255){
+        case (counterValue <= 4):
+            return "No guaranteed IVs, 0% HA Chance, 1/4096 (No charm) 1/1365 (With charm)";
+        case (counterValue <= 9):
+            return "1 guaranteed IV, 0% HA Chance, 1/4096 (No charm) 1/1365 (With charm)";
+        case (counterValue <= 19):
+            return "2 guaranteed IVs, 5% HA Chance, 1/819 (No charm) 1/683 (With charm)";
+        case (counterValue <= 29):
+            return "3 guaranteed IVs, 10% HA Chance, 1/585 (No charm) 1/455 (With charm)";
+        case (counterValue < 255):
+            return "4 perfect IVs, 15% HA chance, 1/315 (No charm) 1/273 (With charm)";
+        case (counterValue === 255):
+            return "4 guaranteed IVs, 15% HA Chance,  1/315 (No charm) 1/273 (With charm) (note - your chain has been reset to zero in-game if playing Sun or Moon)";
+        case (counterValue === max || counterValue === min):
+            return "LIMIT reached!";
+    }
+}
+function updateNightMode(isNightMode) {
+    if (isNightMode === true || isNightMode === 'true') {
+        document.getElementsByTagName('body')[0].classList.add('night');
+    } else {
+        document.getElementsByTagName('body')[0].classList.remove('night');
+    }
+}
+
+window.onload = function() {
+    let nightMode = window.localStorage['nightMode'] === 'true';
+    updateNightMode(nightMode);
+
+    document.getElementById('night-mode').onclick = () => {
+        nightMode = !nightMode;
+        window.localStorage['nightMode'] = nightMode;
+        updateNightMode(nightMode);
+    }
+}
+
+// hide extended portfolio until clicked
+function toggler(divId) {
+    $("#" + divId).slideToggle(300);
+}
+
+function togglerclose(divId) {
+    $("#" + divId).slideToggle(100);
+}
+
+window.addEventListener('keyup', function(event) {
+    const key = event.keyCode;
+    if (key === 87 || key === 38) {
+        // 'w' or 'up arrow'
+        incrementCounterValueBy(1);
+    } else if (key === 83 || key === 40) {
+        // 's' or 'down arrow'
+        incrementCounterValueBy(-1);
+    } else if (key === 82) {
+        // 'r'
+        incrementCounterValueBy(-getCounterValue())
+    }
+    updateMessage();
+})
